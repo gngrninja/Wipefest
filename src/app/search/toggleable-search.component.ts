276b590c1103dff19319ccf1cb9b00424d3b0bcf
JoshYaxley <joshyaxley93@gmail.com>
2017-06-13 20:34:23 +0100

    ToggleableSearch now displays in a modal

--- src/app/search/toggleable-search.component.ts
+++ src/app/search/toggleable-search.component.ts
@@ -1,17 +1,35 @@
 ﻿import { Component, Input } from '@angular/core';
+import { NgbModal, NgbModalOptions } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'toggleable-search',
-  template: `<div class="ml-2 mb-3">
-    <button class="btn btn-sm btn-outline-info" (click)="showSearch = !showSearch"><i class="fa fa-search"></i> {{text}}</button>
-  </div>
+    selector: 'toggleable-search',
+    template: `
+<div class="ml-2 mb-3">
+  <button class="btn btn-sm btn-outline-secondary" (click)="open(content)"><i class="fa fa-search"></i> {{text}}</button>
+</div>
 
-  <search *ngIf="showSearch"></search>`,
-  styles: [``]
+<ng-template #content let-c="close" let-d="dismiss">
+  <div class="modal-header">
+    <h4 class="modal-title">Search</h4>
+    <button type="button" class="close" aria-label="Close" (click)="d('Cross click')">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="modal-body">
+    <search></search>
+  </div>
+</ng-template>
+`,
+    styles: [``]
 })
 export class ToggleableSearchComponent {
 
-    showSearch = false;
     @Input() text = "Search";
+
+    constructor(private modal: NgbModal) { }
+
+    open(content) {
+        this.modal.open(content, { size: "lg" });
+    }
 
 }
