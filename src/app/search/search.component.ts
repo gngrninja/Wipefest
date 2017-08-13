2b2907540ff902a858fb552ee8e25c8ab1d88a01
JoshYaxley <joshyaxley93@gmail.com>
2017-08-13 15:05:35 +0100

    Search by WarcraftLogs URL rather than report id

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -21,7 +21,8 @@ export class SearchComponent implements OnInit {
     guildRegion: string;
     favouriteGuildIsSet: boolean;
 
-    reportId = "";
+    warcraftLogsLink = "";
+    warcraftLogsLinkError = "";
 
     ngOnInit() {
         this.character = this.localStorage.get("character") || "";
@@ -79,14 +80,46 @@ export class SearchComponent implements OnInit {
         }
     }
 
-    searchByReport() {
-        if (this.reportId.trim()) {
-            this.router.navigate([`/report/${this.clean(this.reportId)}`]);
+    searchByLink() {
+        this.router.navigate([`${this.parseLink()}`]);
+    }
+
+    parseLink() {
+        let reportResults = this.warcraftLogsLink.match("\/reports\/[a-zA-Z0-9]*");
+
+        if (!(reportResults && reportResults.length > 0)) {
+            return null;
+        }
+        
+        let reportId = reportResults[0].replace("\/reports\/", "");
+
+        let fightResults = this.warcraftLogsLink.match("fight=[0-9]*");
+
+        if (!(fightResults && fightResults.length > 0)) {
+            return `/report/${reportId}`;
+        }
+
+        let fightId = fightResults[0].replace("fight=", "");
+
+        return `/report/${reportId}/fight/${fightId}`;
+    }
+
+    validateLink() {
+        if (this.warcraftLogsLink == "") {
+            this.warcraftLogsLinkError = "";
+            return;
+        }
+
+        let route = this.parseLink();
+        if (route) {
+            this.warcraftLogsLinkError = "";
+        } else {
+            this.warcraftLogsLinkError = "Invalid link";
         }
     }
 
     clean(input: string): string {
-        return input.trim().replace(/ /g, "-");
+        return input.trim().replace(/ /g, "-").replace(/'/g, "");
     }
 
 }
