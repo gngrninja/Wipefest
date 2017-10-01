e30b6ac460ac5da7f80e7d95447cdfe3bfcc8e70
JoshYaxley <joshyaxley93@gmail.com>
2017-10-01 13:20:53 +0100

    Tip formatting. Favicon.

--- src/app/insights/custom/tomb-of-sargeras/sisters-of-the-moon-insights.ts
+++ src/app/insights/custom/tomb-of-sargeras/sisters-of-the-moon-insights.ts
@@ -9,7 +9,7 @@ import { PlayerAndDuration } from "app/insights/models/player-and-duration";
 import { Ability } from "app/fight-events/models/ability-event";
 
 export module SistersOfTheMoonInsights {
-    
+
     export function TwilightGlaive(events: FightEvent[]): Insight {
         let debuffEvents = events
             .filter(x => x.config)
@@ -36,7 +36,17 @@ export module SistersOfTheMoonInsights {
 
         let insight = `You unnecessarily intercepted ${MarkupHelper.AbilityWithIcon(damageEvents[0].ability)} ${MarkupHelper.Info(totalHits)} times.`;
         let details = MarkupHelper.PlayersAndFrequency(playersAndHits);
-        let tip = `Huntress Kasparian debuffs a player with Twilight Glaive, before firing a glaive at them that travels from Kasparian, to the player, and back. The glaive will change direction if either party moves. The targeted player can't avoid taking damage from it, but anyone else who intercepts it will take unnecessary (and potentially fatal) damage. The targeted player should move to a position so that the glaive is going to intercept as few people as possible (ideally so that the glaive doesn't pass through melee at all), but all raid members should be aware of the path (you can watch the arrow at the feet of Kasparian). Twilight Glaive occurs roughly every 20 seconds, and prefers to target ranged players.`;
+        let tip = `${MarkupHelper.Style("boss", "Huntress Kasparian")} debuffs a player with ${MarkupHelper.Style("arcane", "Twilight Glaive")},
+before firing a glaive at them that travels from ${MarkupHelper.Style("boss", "Huntress Kasparian")}, to the player, and back.
+The glaive will change direction if either party moves.
+The targeted player can't avoid taking damage from it,
+but anyone else who intercepts it will take unnecessary (and potentially fatal) damage.
+The targeted player should move to a position so that the glaive is going to intercept as few people as possible
+(ideally so that the glaive doesn't pass through melee at all),
+but all raid members should be aware of the path
+(you can watch the arrow at the feet of ${MarkupHelper.Style("boss", "Huntress Kasparian")}).
+${MarkupHelper.Style("arcane", "Twilight Glaive")} occurs roughly every 20 seconds,
+and prefers to target ranged players.`;
 
         return new Insight(insight, details, tip);
     }
@@ -57,7 +67,16 @@ export module SistersOfTheMoonInsights {
 
         let insight = `You gained ${MarkupHelper.Info(6)} stacks of ${MarkupHelper.AbilityWithIcon(debuffEvents[0].ability)} ${MarkupHelper.Info(totalFrequency)} times.`;
         let details = `The sixth stack was gained from: ${MarkupHelper.PlayersAndFrequency(playersAndFrequency)}`;
-        let tip = "Every time a player crosses the line, the raid takes damage from Astral Purge, and gains a stack of Astral Vulnerability (which increases the damage taken from Astral Purge). Astral Vulnerability expires after 2 seconds. To minimise raid damage, raids often cross the line in groups of 5 or less players. Take care not to stand on the line or to strafe back and forth across it. Be wary in melee-heavy compositions, where a large proportion of the raid will necessarily be clumped up. Melee can split into two groups, and should be careful not to follow the boss across the line unnecessarily, even if the tanks move them out of range.";
+        let tip = `Every time a player crosses the line,
+the raid takes damage from ${MarkupHelper.Style("spellshadow", "Astral Purge")},
+and gains a stack of ${MarkupHelper.Style("physical", "Astral Vulnerability")}
+(which increases the damage taken from ${MarkupHelper.Style("spellshadow", "Astral Purge")}).
+${MarkupHelper.Style("physical", "Astral Vulnerability")} expires after 2 seconds.
+To minimise raid damage, raids often cross the line in groups of 5 or less players.
+Take care not to stand on the line or to strafe back and forth across it.
+Be wary in melee-heavy compositions, where a large proportion of the raid will necessarily be clumped up.
+Melee can split into two groups, and should be careful not to follow the boss across the line unnecessarily,
+even if the tanks move them out of range.`;
 
         return new Insight(insight, details, tip);
     }
@@ -95,7 +114,12 @@ export module SistersOfTheMoonInsights {
 
         let insight = `You had a total ${MarkupHelper.AbilityWithIcon(debuffEvents[0].ability)} duration of ${MarkupHelper.Info(Timestamp.ToSeconds(totalDuration))}, with ${MarkupHelper.Info(playersAndDurationsOverThreshold.length)} lasting longer than ${Timestamp.ToSeconds(threshold)}.`;
         let details = MarkupHelper.PlayersAndDurations(playersAndDurationsOverThreshold);
-        let tip = "Moon Burn is a 30 second long debuff that deals damage every 2 seconds. Players can remove it by crossing the line (triggering Astral Purge). On Mythic, make sure to watch the raid's current Astral Vulnerability stacks before crossing the line. Crossing at low or zero stacks will reduce unnecessary raid damage. When teams split into groups of 5 or less to deal with Astral Vulnerability, they typically decide to drop Moon Burn debuffs inbetween these groups.";
+        let tip = `${MarkupHelper.Style("arcane", "Moon Burn")} is a 30 second long debuff that deals damage every 2 seconds.
+Players can remove it by crossing the line (triggering ${MarkupHelper.Style("spellshadow", "Astral Purge")}).
+On Mythic, make sure to watch the raid's current ${MarkupHelper.Style("physical", "Astral Vulnerability")} stacks before crossing the line.
+Crossing at low or zero stacks will reduce unnecessary raid damage.
+When teams split into groups of 5 or less to deal with ${MarkupHelper.Style("physical", "Astral Vulnerability")},
+they typically decide to drop ${MarkupHelper.Style("spellshadow", "Astral Purge")} debuffs inbetween these groups.`;
 
         return new Insight(insight, details, tip);
     }
