cbbd3ff6cca604a3ad6c8cbdb11dffc3f6f54336
JoshYaxley <joshyaxley93@gmail.com>
2018-01-20 18:43:01 +0000

    Bug fix - avoid splitting null string

--- src/app/insights/models/insight.ts
+++ src/app/insights/models/insight.ts
@@ -7,10 +7,10 @@ export class Insight {
     }
 
     get details(): string {
-        return this._details.split("\r").join("").split("\n").join("") == "." ? null : this._details;
+        return this._details == null ? null : this._details.split("\r").join("").split("\n").join("") == "." ? null : this._details;
     }
 
     get tip(): string {
-        return this._tip.split("\r").join("").split("\n").join("") == "." ? null : this._tip;
+        return this._tip == null ? null : this._tip.split("\r").join("").split("\n").join("") == "." ? null : this._tip;
     }
 }
