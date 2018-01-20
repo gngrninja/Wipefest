079baee79a394ceddd9e2a374a15564ee3eb2a64
JoshYaxley <joshyaxley93@gmail.com>
2018-01-20 17:04:31 +0000

    Varimathras Echoes of Doom Insight config

--- src/app/insights/configs/antorus-the-burning-throne/varimathras/all.ts
+++ src/app/insights/configs/antorus-the-burning-throne/varimathras/all.ts
@@ -21,6 +21,15 @@ export module VarimathrasInsightConfigs {
             // Necrotic Embrace spreads
 
             new HitUnlessRole("0", 2069, ["Shadow Strike"], [243960], "Tank"),
+            new Debuff("4", 2069, ["Echoes of Doom"], [248732], null, `<p>{abilitiesAndTimestamps}</p>{playersAndFrequencies}`, `
+On mythic difficulty, getting hit by {ability:243963:Alone in the Darkness:shadow}
+will spawn a {npc:Shadow of Varimathras}.
+This add will cast {ability:248732:Echoes of Doom:shadow},
+debuffing a player with {ability:248732:Echoes of Doom:shadow},
+which, when it expires, leaves a pool at the player's feet that deals shadow damage.
+Because these pools deal shadow damage, they will trigger {ability:243961:Misery:shadow} if stepped in,
+so need to be placed safely away from the raid.
+Aim to kill all {npc:Shadow of Varimathras} adds before they can successfully cast {ability:248732:Echoes of Doom:shadow}.`),
             new MarkedPrey("1"),
             new Hit("2", 2069, ["Dark Fissure"], [244005]),
             new HitWithoutDebuff("3", 2069, ["Alone in the Darkness"], [243963], ["Necrotic Embrace"], [244094], 10000)
