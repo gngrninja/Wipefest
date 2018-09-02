5b79bad275bb9fc43aa138cbc503e5e53afc28e7
JoshYaxley <joshyaxley93@gmail.com>
2018-09-02 20:40:01 +0100

    Uldir zone on Character page

--- src/app/character-search-results/character-search-results.component.ts
+++ src/app/character-search-results/character-search-results.component.ts
@@ -23,8 +23,7 @@ export class CharacterSearchResultsComponent implements OnInit {
   region: string;
 
   zones: Zone[] = [
-    new Zone('Tomb of Sargeras', 'sargeras', 13, [1, 2, 3]),
-    new Zone('Antorus, the Burning Throne', 'antorus', 17, [1])
+    new Zone('Uldir', 'uldir', 19, [1])
   ];
   selectedZone: Zone;
   encounters: ParsesForEncounter[] = [];
@@ -87,7 +86,7 @@ export class CharacterSearchResultsComponent implements OnInit {
     this.realm = params.realm || this.localStorage.get('characterRealm');
     this.region = params.region || this.localStorage.get('characterRegion');
     this.selectedZone =
-      this.zones.find(x => x.slug === params.zone) || this.zones[1];
+      this.zones.find(x => x.slug === params.zone) || this.zones[0];
     this.encounters = [];
 
     if (!this.character || !this.realm || !this.region) {
